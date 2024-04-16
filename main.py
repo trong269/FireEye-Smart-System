c9fcf8e19ac4bb8790ac1e824c04f9f9ab9a197e
Nguyen Trong <trongbg2692004@gmail.com>
2024-04-17 02:16:02 +0700

    add warning sound

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import cv2
 from ultralytics import YOLO
 import numpy as np
 import time
+import pyglet
 
 model = YOLO(r"Model\best.pt")
 COLOR = (0,0,255)
@@ -31,7 +32,12 @@ while True:
     if check and (end_time - start_time) >= 60 :
         start_time = time.time()
         cv2.imwrite('warning.png', img)
+        # Play file sound
+        music = pyglet.resource.media('chuongbaochay.mp3')
+        music.play()
+        # #send image
         send_warning(img_path= 'warning.png')
+        #send message
         send_message()
     if cv2.waitKey(1) == ord('q'):
         break
